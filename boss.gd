7b80bd748cb8e6059189b50c091da9ee1c9e9de9
Lumpycrow <147742864+LumpyCrow@users.noreply.github.com>
2024-10-22 10:44:56 -0700

    adding the boss' weapons

--- boss.gd
+++ boss.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
+var LittleLazer = load("res://mini_lazer.tscn")
 
-const SPEED = 50.0
+const SPEED = 20.0
 var player = null
 var player_chase = false
 
 func _physics_process(delta: float) -> void:
+	$lazergunPivot.rotation = $lazergunPivot.rotation + 0.05
 	if player_chase:
 		velocity += (player.position - position)/SPEED
 
@@ -16,3 +18,6 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player = body
 		player_chase = true
+		
+func _on_minilazer_cooldown_timeout() -> void:
+	pass # Replace with function body.
