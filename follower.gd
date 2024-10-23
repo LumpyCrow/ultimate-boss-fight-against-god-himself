66e72893693e90d494ff966f5fe885ca62c96a3a
Lumpycrow <147742864+LumpyCrow@users.noreply.github.com>
2024-10-23 13:15:02 -0700

    adding enemies that the boss will spawn

---
+++ follower.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+@onready var player = get_parent().get_node("player")
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	position += transform.x * SPEED * delta
+	look_at(player.position)
+	move_and_slide()
